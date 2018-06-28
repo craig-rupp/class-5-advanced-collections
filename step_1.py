0c013569e483970a1c2983ceaf95d1b6e09f2b34
Craig Rupp <craigrupp@utexas.edu>
2018-06-28 23:23:01 +0000

    First Two Steps Done

--- step_1.py
+++ step_1.py
@@ -1,2 +1,10 @@
+from data import products_string
+from pprint import pprint 
 def transform_products_to_list(products_string):
-    pass
+    string_list = products_string.split('\n')
+    new_string_list = [x for x in string_list if x != '']
+    final_list = []
+    for i in range(len(new_string_list)):
+        final_list.append(new_string_list[i].split(','))
+    return final_list
+pprint(transform_products_to_list(products_string))
--- step_2.py
+++ step_2.py
@@ -1,5 +1,12 @@
 from step_1 import transform_products_to_list
-
-
+from data import products_string
+from pprint import pprint 
 def group_products_by_customer(products_string):
-    pass
+    list_items = transform_products_to_list(products_string)
+    step_2_dict = {}
+    for item in list_items:
+        step_2_dict.setdefault(item[-2], [])
+        step_2_dict[item[-2]].append(item)
+    return step_2_dict
+
+pprint(group_products_by_customer(products_string))
